c675dc643adba0bcf20d6894463b8670f8735277
adeeb5378 <adeebkhan0671@gmail.com>
2022-12-22 11:57:50 +0530

    Completed project

---
+++ script.js
@@ -0,0 +1,23 @@
+// let x = new Date();
+// let x = new Date().getFullYear();
+// let x = new Date().getHours();
+// console.log(x);
+// setInterval(()=>{
+//     console.log("Hi Everyone!");
+// },1000);
+
+setInterval(()=>{
+    d = new Date();
+    hTime = d.getHours();
+    mTime = d.getMinutes();
+    sTime = d.getSeconds();
+    
+    hRotation = 30*hTime + mTime/2;
+    mRotation = 6*mTime;
+    sRotation = 6*sTime;
+
+    hour.style.transform = `rotate(${hRotation}deg)`;
+    minute.style.transform = `rotate(${mRotation}deg)`;
+    second.style.transform = `rotate(${sRotation}deg)`;
+
+},1000);
